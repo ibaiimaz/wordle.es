1603ab321ddd7e2c7210872f093cb03ef939511e
Daniel Rodriguez <daniel@danielfrg.com>
2022-01-09 19:57:14 -0600

    Remove cookie deps

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import { CookiesProvider } from "react-cookie";
 import { GameContextProvider } from "../components/GameContext";
 import SiteConfig from "../lib/config";
 import "../styles/globals.css";
@@ -8,18 +7,16 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <CookiesProvider>
-        <GameContextProvider>
-          <Head>
-            <title>{SiteConfig.title}</title>
-            <meta
-              name="viewport"
-              content="minimum-scale=1, initial-scale=1, width=device-width"
-            />
-          </Head>
-          <Component {...pageProps} />
-        </GameContextProvider>
-      </CookiesProvider>
+      <GameContextProvider>
+        <Head>
+          <title>{SiteConfig.title}</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </GameContextProvider>
     </>
   );
 }
