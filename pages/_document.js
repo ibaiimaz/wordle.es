93534cfd71b4e13aef271e9ad88c9219d07c29b4
Daniel Rodriguez <daniel@danielfrg.com>
2022-01-09 22:06:04 -0600

    Dios llevame

--- pages/_document.js
+++ pages/_document.js
@@ -17,17 +17,17 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"></meta>
           <meta name="description" content={`${SiteConfig.desc}`}></meta>
 
-          <meta property="og:url" content="https://danielfrg.wordle.com"></meta>
+          <meta property="og:url" content="https://wordle.danielfrg.com"></meta>
           <meta property="og:type" content="website"></meta>
           <meta property="og:title" content={`${SiteConfig.title}`}></meta>
           <meta property="og:description" content={`${SiteConfig.desc}`}></meta>
           <meta
             property="og:image"
-            content="https://danielfrg.wordle.com/images/wordle_og_1200x630.jpg"
+            content="https://wordle.danielfrg.com/images/wordle_og_1200x630.jpg"
           ></meta>
 
           <meta name="twitter:card" content="summary_large_image"></meta>
-          <meta property="twitter:domain" content="danielfrg.wordle.com"></meta>
+          <meta property="twitter:domain" content="wordle.danielfrg.com"></meta>
 
           <meta name="theme-color" content="#6aaa64"></meta>
           <link rel="manifest" href="manifest.json" />
