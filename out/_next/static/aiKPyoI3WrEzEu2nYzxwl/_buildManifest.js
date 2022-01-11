da5298a1b3a0631bb501ae8bebe67351d6305eba
Ibai Imaz <ibaiimaz@gmail.com>
2022-01-11 14:22:52 +0100

    test

---
+++ out/_next/static/aiKPyoI3WrEzEu2nYzxwl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bf0fd529c36bb636.css","static/chunks/930-6823f9f00a3b611e.js","static/chunks/pages/index-ac9a4134b37a122f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
